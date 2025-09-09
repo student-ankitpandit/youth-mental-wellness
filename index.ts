3b60148df5a10eb1820c55c1f79d2231dcc1ebfc
Student-ankitpandit <Student.ankitkumarpandit1221@gmail.in>
2025-09-09 19:00:48 +0530

    init setup

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from "express"
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req:any, res:any) => {
+    res.send("Hello from app")
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 3000")
+})
+
+
